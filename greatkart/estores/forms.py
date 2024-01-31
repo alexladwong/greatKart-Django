65ce6849ac608bd32cc12041e87685bf4e494f69
alexladwong <alexladwong@gmail.com>
2024-01-31 23:09:11 +0300

    ratings/imgsligder

--- greatkart/estores/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ReviewRating
-
-
-class ReviewForm(forms.ModelForm):
-    class Meta:
-        model = ReviewRating
-        fields = ["subject", "review", "rating"]
--- greatkart/orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("place_order/", views.Place_Orders, name="place_order"),
-    path("payments/", views.Payments, name="payments"),
-    path("order-completed/", views.OrderCompleted, name="order_completed"),
-]
