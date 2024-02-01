8f562d3608e8e60c786ec3ae3acdaebe61488a2d
alexladwong <alexladwong@gmail.com>
2024-02-01 20:40:31 +0300

    honeypot01

--- greatKart-Local/greatkart/greatkart/settings.py
+++ greatKart-Local/greatkart/greatkart/settings.py
@@ -61,7 +61,14 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_session_timeout.middleware.SessionTimeoutMiddleware",
 ]
+# Session timeout middleware
+SESSION_EXPIRE_SECONDS = 300  # 60 seconds logout automatically
+
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = "/accounts/login/"
+
 
 ROOT_URLCONF = "greatkart.urls"
 
